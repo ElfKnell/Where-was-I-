dd43a231b45b218b94cd5573629d5ef6727147c3
Andrii Kyrychenko <akyrych84@gmail.com>
2022-06-14 14:44:34 -0400

    Initial Commit

---
+++ Where was I?/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Where was I?
+//
+//  Created by Andrii Kyrychenko on 14/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
