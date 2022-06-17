aaecc5540db8251bacf985d52a30bd88aef5b7b5
Andrii Kyrychenko <akyrych84@gmail.com>
2022-06-16 20:57:25 -0400

    Added the ability to set coordinates and mark them.

--- Where was I?/ViewController.swift
+++ Where was I?/ViewController.swift
@@ -11,6 +11,9 @@ import MapKit
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var longTextBox: UITextField!
+    @IBOutlet weak var latTextBox: UITextField!
 
     @IBAction func saveButtonClick(_ sender: Any) {
         let coordinate = locationManager.location?.coordinate
@@ -20,23 +23,46 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         DataStore().storeDatePoin(lanitude: String(lat), longitude: String(long))
         getAnotation()
-        //setCoordinate()
+        
     }
     
-    func setCoordinate() {
+    @IBAction func markButtonClick(_ sender: Any) {
+        
+        guard let latitudeWant = Float(longTextBox.text ?? "") else { return }
+        guard let longitudeWant = Float(latTextBox.text ?? "") else { return }
+        let locLat = CLLocationDegrees(latitudeWant)
+        let locLong = CLLocationDegrees(longitudeWant)
+        
+        let anotation = MKPointAnnotation()
+        anotation.coordinate.latitude = locLat
+        anotation.coordinate.longitude = locLong
+        
+        anotation.title = "I want to here!"
+        mapView.addAnnotation(anotation)
+    }
+    
+    @IBAction func goButtonClick(_ sender: Any) {
+        guard let lat = Float(longTextBox.text ?? "") else { return }
+        guard let long = Float(latTextBox.text ?? "") else { return }
+        if (lat < -90.0 || lat > 90.0) || (long < -180.0 || long > 180.0) {
+            return
+        } else {
+            setCoordinate(latDegrees: lat, longDegrees: long)
+        }
+        
+    }
+    
+    func setCoordinate(latDegrees: Float, longDegrees: Float) {
         // Set initial location in Honolulu
-        let locLat = CLLocationDegrees(21.282778)
-        let locLong = CLLocationDegrees(-157.829444)
+        let locLat = CLLocationDegrees(latDegrees)
+        let locLong = CLLocationDegrees(longDegrees)
         let initialLocation = CLLocationCoordinate2D(latitude: locLat, longitude: locLong)
-        let disLat = CLLocationDistance(300)
-        let disLong = CLLocationDistance(300)
+        let disLat = CLLocationDistance(400)
+        let disLong = CLLocationDistance(400)
         let coor = MKCoordinateRegion(center: initialLocation, latitudinalMeters: disLat, longitudinalMeters: disLong)
         
         mapView.setRegion(coor, animated: true)
         
-        
-        
-
     }
     
     @IBOutlet weak var mapView: MKMapView!
@@ -53,6 +79,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         getAnotation()
     }
     
+    
+    @IBAction func longitudeTextBox(_ sender: Any) {
+    }
+    @IBAction func latitudeTextBox(_ sender: Any) {
+    }
     func getAnotation() {
         guard let oldCoords = DataStore().getLastLocation() else { return }
         let anotation = MKPointAnnotation()
